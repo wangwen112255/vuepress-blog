1637b517b836c361011e99f54d92ff04bacb0596
wangwen112255 <1205483453@qq.com>
2022-10-10 11:29:31 +0800

    打包测试4

--- deploy.sh
+++ deploy.sh
@@ -48,5 +48,6 @@
 #/usr/bin/env: node: No such file or directory
 #/usr/bin/env: node: No such file or directory
 #/usr/bin/env: node: No such file or directory
+#/usr/bin/env: node: No such file or directory
 
 
