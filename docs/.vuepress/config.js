40efab5553fb5aff48355eaf7630521efe9d8276
wangwen112255 <1205483453@qq.com>
2022-10-10 10:07:55 +0800

    修改目录前缀

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,8 @@ const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
-  // base: '/', // 格式：'/<仓库名>/'， 默认'/'
-  base: '/blog/',  // 服务器打包前需要注释掉因为是根目录，github是部署在blog下
+  base: '/', // 格式：'/<仓库名>/'， 默认'/'
+  // base: '/blog/',  // 服务器打包前需要注释掉因为是根目录，github是部署在blog下
   // theme: 'vdoing', // 使用npm包主题
   theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
